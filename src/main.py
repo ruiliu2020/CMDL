2621e97ac49fb80fea5a17f85a568c2db23f5734
ruiliu2020 <56811374+ruiliu2020@users.noreply.github.com>
2024-10-29 15:25:45 +0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import os
+import argparse
+from utils.quick_start import quick_start
+os.environ['NUMEXPR_MAX_THREADS'] = '48'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='CMDL', help='name of models')
+    parser.add_argument('--dataset', '-d', type=str, default='clothing', help='name of datasets')
+
+    config_dict = {
+        'gpu_id': 0,
+    }
+
+    args, _ = parser.parse_known_args()
+
+    quick_start(model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True)
+
+
